a42413cdbeb6f2c32278b050a16d79a49770ab40
buenoleo67 <111444675+buenoleo67@users.noreply.github.com>
2022-11-20 20:02:57 -0300

    Update sobre.php

--- sobre.php
+++ sobre.php
@@ -8,21 +8,35 @@
     <title>Sobre Nós</title>
 </head>
 <body bgcolor="black">
-    <div>
-    <h1>Sobre nós</h1>
-    <h4>Focados em lhes trazer sempre o melhor serviço possível</h4>
-    <h1>Assistência equipada com ferramentas para tornar a manutenção o mais perfeito possível</h1>
-    <img src="imggpu.jpg.webp" alt="processador2">      <img src="bancada.jpg" alt="bancada tecnico">
-    <h1>Ferramentas de última geração</h1>
-    <img src="microscopio" alt="microscopio"> <img src="estacaosolda.jpg" alt="estação de solda"> <img src="gpu.jpg" alt="processador">
+    <main>
+        <div class="header-2">
+           <div class="menu">
+             <ul>
+                <li><a href="index.php"> Home</a><br></li>
+                <li> <a href="login.php" target = "blank">Aréa de login</a></li>
+                <li><a href="cadastro.php" target = "blank">Aréa de cadastro</a></li>        
+             </ul>
+           </div>
+        </div>
+       </main>
+     <div class="pg1">
+         <h1>Quem é a ZTECH ? </h1>
+         <a class="sub-pg"> 
+            Há mais de 5 anos no mercado prestando serviços especializados em TV's, Celulares, Tablets e Computadores.
+             Trabalhamos com os mais variados tipos de acessorios de informatica e de celulares.  <br></a>
+     </div>
+     <div class="pg2">
+         <h1>
+            Hoje temos a bancada composta pelos melhores esquipamentos para ter o melhor resultado possivel do serviço e satisfazendo o clinte final!
+         </h1>
+         <img src="bancada.jpg" alt="bancada tecnico"> <br>
+         <h1>
+             A ZTECH é especializada em reparos avançado na linha Apple e Androids.
+         </h1>
+         <img src="placa.JPG" alt="processador2">      
+
     </div>
-    <div>
-        <ul>
-            <li><a href="index.php"> Home</a><br></li>
-            <li> <a href="login.php" target = "blank">Faça login</a></li>
-            <li><a href="cadastro.php" target = "blank">Faça seu cadastro</a></li>        
-        </ul>
-    </div>
+
     
 </body>
 </html>
